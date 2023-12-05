ac5cb26f882571182168963b5f0d4862aea23fee
ChandrasekaranN <chandrasekaran.n@northeastern.edu>
2023-12-05 17:06:17 -0500

    Signup + JWT auth added

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  fullName: {
+  username: {
     type: String,
     required: true,
     validate: /^[a-zA-Z ]*$/,
@@ -17,6 +18,20 @@ const userSchema = new mongoose.Schema({
     required: true,
     validate: /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$/,
   },
+});
+
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+    user.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
 });
 
 const User = mongoose.model('User', userSchema);
