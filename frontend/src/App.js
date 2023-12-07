cebee7b8ac393e73a438cbd07cee56f221d9c8ca
KKFaiyaz <kadharkani.f@northeastern.edu>
2023-12-06 22:40:02 -0500

    main component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Main from './pages/main/main';
 
 
 const App = () => {
   return (
     <Router>
       <Routes>        
+        <Route path="/" element={<Main/>} />
         <Route path="/login" element={<Login/>} />
-        
       </Routes>
     </Router>
   );
