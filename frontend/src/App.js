ff2c112e386cab6cfd2bd8f14a4c2e3bf9181783
ChandrasekaranN <chandrasekaran.n@northeastern.edu>
2023-12-06 23:44:25 -0500

    Add authentication routes and logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
 import ViewProduct from './pages/ViewProduct';
 import PostItems from './pages/PostItems';
-
+import Signup from './pages/Signup';
+import Logout from './pages/Logout';
 
 const App = () => {
+  const [hasAccessToken, setHasAccessToken] = useState(false);
+
+  useEffect(() => {
+    const accessTokenCookie = document.cookie.split(';').find(cookie => cookie.trim().startsWith('userAccessToken='));
+    
+    if (accessTokenCookie) {
+      setHasAccessToken(true);
+    }
+  },[])
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/listing/:id" element={<ViewProduct/>} />
-        <Route path="/postItems" element={<PostItems/>} />
-        <Route path="/login" element={<Login/>} />
+      {hasAccessToken ? (
+        <>
+          <Route path="/" element={<Home/>} />
+          <Route path="/listing/:id" element={<ViewProduct/>} />
+          <Route path="/postItems" element={<PostItems/>} />
+          <Route path="/logout" element={<Logout/>} />
+        </>
+        ) : (
+        <>
+        </>
+        )}
+          <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
       </Routes>
     </Router>
   );
