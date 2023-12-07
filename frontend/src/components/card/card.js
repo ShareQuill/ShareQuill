a6a8186d5f9ab0a3458f046b1fdeb0f7c77752ab
ChandrasekaranN <chandrasekaran.n@northeastern.edu>
2023-12-07 15:30:41 -0500

    Refactor card component and update styles

--- frontend/src/components/card/card.js
+++ frontend/src/components/card/card.js
@@ -18,28 +18,20 @@ function ProductCard() {
           img: 'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-980x653.jpg',
           description: 'Description for Item 3. Yet another sample description.',
         },
-        {
-          title: 'Item 4',
-          img: 'https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-980x653.jpg',
-          description: 'Description for Item 4. Yet another sample description.',
-        },
       ];
   return (
     <div className='container d-flex space-between'>
     {items.map((product, index) => (
-        <>
-        <Card style={{ width: '18rem'}}>
-            <Card.Img variant="top" src={product.img} />
-            <Card.Body>
-            <Card.Title>{product.title}</Card.Title>
-            <Card.Text>
-            {product.description}
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
-        
-        </>
+          <Card style={{ width: '18rem'}}>
+              <Card.Img variant="top" src={product.img} />
+              <Card.Body>
+              <Card.Title>{product.title}</Card.Title>
+              <Card.Text>
+              {product.description}
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+          </Card>
     ))}
     </div>
   );
