3cccd8da90d7d8815d3d4390b9e6bd9ece0345f0
KKFaiyaz <kadharkani.f@northeastern.edu>
2023-12-07 01:59:58 -0500

    card animations

--- frontend/src/components/card/card.js
+++ frontend/src/components/card/card.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Tilt } from 'react-tilt';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
@@ -24,24 +26,23 @@ function ProductCard() {
           description: 'Description for Item 4. Yet another sample description.',
         },
       ];
-  return (
-    <div className='container d-flex space-between'>
-    {items.map((product, index) => (
-        <>
-        <Card style={{ width: '18rem'}}>
-            <Card.Img variant="top" src={product.img} />
-            <Card.Body>
-            <Card.Title>{product.title}</Card.Title>
-            <Card.Text>
-            {product.description}
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
-        
-        </>
-    ))}
-    </div>
+      return (
+        <div className='container d-flex space-between'>
+          {items.map((product, index) => (
+            <Tilt key={index} options={{ max: 25, perspective: 1000, scale: 1.1 }}>
+              <Card className='card-display m-md-4 position-relative'>
+                <Card.Img variant="top" src={product.img} />
+                <Card.Body className=''>
+                  <Card.Title className='d-inline'>{product.title}</Card.Title>
+                  <Card.Text>{product.description}</Card.Text>
+                  <Button className='btn btn-outline-light btn-float' variant="primary">
+                    Go somewhere
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Tilt>
+          ))}
+        </div>
   );
 }
 
